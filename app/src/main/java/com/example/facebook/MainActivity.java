b4ecd3a2866f0b55a4af82d72445052db858518f
Divij <divij.edcellpup@gmail.com>
2019-03-16 15:46:04 +0530

    First commit

---
+++ app/src/main/java/com/example/facebook/MainActivity.java
@@ -0,0 +1,168 @@
+package com.example.facebook;
+
+import android.app.DownloadManager;
+import android.content.Intent;
+import android.content.pm.PackageInstaller;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.net.UrlQuerySanitizer;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.Profile;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import com.squareup.picasso.Picasso;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String EMAIL = "email";
+    LoginButton loginButton;
+     CallbackManager callbackManager=CallbackManager.Factory.create();
+     TextView Email,first,last,Gender;
+    String email="",firstName="",lastName="",gender="",link="";
+    ImageView profile_pic;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+       Email=findViewById(R.id.email);
+        first=findViewById(R.id.first_name);
+        last=findViewById(R.id.last_name);
+        Gender=findViewById(R.id.gender);
+        profile_pic=findViewById(R.id.profile_pic);
+        loginButton = (LoginButton) findViewById(R.id.login_button);
+        loginButton.setReadPermissions(Arrays.asList(EMAIL));
+
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        AppEventsLogger.activateApp(this);
+
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Toast.makeText(MainActivity.this, "Success", Toast.LENGTH_SHORT).show();
+                setFacebookData(loginResult);
+
+
+
+
+
+            }
+
+            @Override
+            public void onCancel() {
+                Toast.makeText(MainActivity.this, "Cancelled", Toast.LENGTH_SHORT).show();
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Toast.makeText(MainActivity.this, "Error: "+error, Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void setFacebookData(final LoginResult loginResult)
+    {
+        GraphRequest request = GraphRequest.newMeRequest(
+                loginResult.getAccessToken(),
+                new GraphRequest.GraphJSONObjectCallback() {
+                    @Override
+                    public void onCompleted(JSONObject object, GraphResponse response) {
+                        // Application code
+                        try {
+                            if (object.has("email")){
+                                email=response.getJSONObject().getString("email").trim();
+                            }else{
+                                email="";
+                            }
+                            Email.setText(email);
+                            if (object.has("first_name")){
+                                firstName = response.getJSONObject().getString("first_name").trim();
+                            }
+                            else{
+                                firstName="";
+                            }
+                            first.setText(firstName);
+                            if (object.has("last_name")){
+                                lastName = response.getJSONObject().getString("last_name").toString().trim();
+                            }
+                            else{
+                                lastName="";
+                            }
+                            last.setText(lastName);
+                            if (object.has("gender")){
+                                gender = response.getJSONObject().getString("gender").toString().trim();
+                            }else {
+                                gender="";
+
+                            }
+                            Gender.setText(gender);
+
+                            Profile profile = Profile.getCurrentProfile();
+                            String id = profile.getId();
+                            link = profile.getLinkUri().toString();
+                            Log.i("Link",link);
+                            if (Profile.getCurrentProfile()!=null)
+                            {
+                                Log.i("Login", "ProfilePic" + Profile.getCurrentProfile().getProfilePictureUri(200, 200));
+//                                profile_pic.setImageURI(profile.getProfilePictureUri(200,200));
+                                Picasso.get().load(profile.getProfilePictureUri(200,200)).into(profile_pic);
+                            }
+
+                            Log.i("Login" + "Email", email);
+                            Log.i("Login"+ "FirstName", firstName);
+                            Log.i("Login" + "LastName", lastName);
+                            Log.i("Login" + "Gender", gender);
+
+
+
+
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+        Bundle parameters = new Bundle();
+        parameters.putString("fields", "id,email,first_name,last_name,gender,picture");
+        request.setParameters(parameters);
+        request.executeAsync();
+    }
+
+}
